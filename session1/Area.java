4faf7dac73d81a43a2f5efc70b12046abfb36af4
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-17 17:37:42 +0800

    Add files via upload

---
+++ session1/Area.java
@@ -0,0 +1,10 @@
+public class Area {
+	public static void main(String[] args) {
+		int r = 10;
+		double pi = 3.14;
+		double area = r * r * pi;
+		System.out.println("r=" + r);
+		System.out.println("pi=" + pi);
+		System.out.println("area=" + area);
+	}
+}
